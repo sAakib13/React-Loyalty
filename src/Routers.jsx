c3d69531045694edda420aa4a54e1a881f856363
mabdah <mabisha@telerivet.com>
2024-12-02 16:04:48 +0545

    otp page

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 import Login from "./component/login/login"; // Create this component
 import Welcome from "./component/Welcome/Welcome";
 import RegistrationForm from "./component/register/register";
+import OTP from "./component/otp/otp";
 // import About from "./About"; // Optional if needed
 // import Services from "./Services"; // Optional if needed
 
@@ -13,6 +14,7 @@ function Routers() {
       <Route path="/login" element={<Login />} />
       <Route path="/welcome" element={<Welcome />} />
       <Route path="/register" element={<RegistrationForm />} />
+      <Route path="/otp" element={<OTP />}></Route>
       {/* <Route path="/services" element={<Services />} /> */}
     </Routes>
   );
